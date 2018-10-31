79f056382e5989a27196070bc0269d636a5d59a2
Eudaemonal <eudaemonal@gmail.com>
2018-10-31 07:19:24 -0700

    add cpp_template_proj try catch block

--- cpp_template/cpp_template_proj/test01.cpp
+++ cpp_template/cpp_template_proj/test01.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
 int main(int argc, char *argv[]){
+    try{
 
-	return 0;
+        return EXIT_SUCCESS;
+    }catch(std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
 }
